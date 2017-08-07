91d00222d692b80a8eb22f0f193479545846ac89
Yuval Adam <_@yuv.al>
2017-08-07 22:01:15 +0300

    Implement freeze.py

--- freeze.py
+++ freeze.py
@@ -1,7 +1,21 @@
 from flask_frozen import Freezer
-from app import app
+from app import app, BLOG_CONTENT_DIR, WIKI_CONTENT_DIR
+from os import listdir
 
 freezer = Freezer(app)
+
+@freezer.register_generator
+def blog_post():
+    for f in listdir(BLOG_CONTENT_DIR):
+        y, m, d, *title = f[:-3].split('-')
+        slug = '-'.join(title)
+        yield {'y': y, 'm': m, 'd': d, 'slug': slug}
+
+@freezer.register_generator
+def wiki_page():
+    for f in listdir(WIKI_CONTENT_DIR):
+        slug = f[:-3]
+        yield {'slug': slug}
 
 if __name__ == '__main__':
     freezer.freeze()
