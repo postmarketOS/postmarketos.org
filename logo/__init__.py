29862095b5b6e4bcee0df50f46bc521736f98a43
Martijn Braam <martijn@brixit.nl>
2017-08-09 11:29:44 +0200

    Added logo.create_rectangular()

--- logo/__init__.py
+++ logo/__init__.py
@@ -42,7 +42,14 @@ def _midpoint(a, b):
     return _point_between(a, b, 0.5)
 
 
-def _create_polygon(thickness, offset, space, pointyness):
+def _create_polygon(points):
+    pointstring = ""
+    for p in points:
+        pointstring += str(p) + " "
+    return '<polygon points="{}" fill="#090" />'.format(pointstring.strip())
+
+
+def _create_triangle_side(thickness, offset, space, pointyness):
     height = _triangle_height(100)
 
     angle = _point_angle(Point(0, 0), Point(50, height))
@@ -64,10 +71,30 @@ def _create_polygon(thickness, offset, space, pointyness):
         Point(thickness * 1.75, thickness)
     ]
 
-    pointstring = ""
-    for p in polygon:
-        pointstring += str(p) + " "
-    return '<polygon points="{}" fill="#090" />'.format(pointstring.strip())
+    return _create_polygon(polygon)
+
+
+def _create_square_side(thickness, offset, space, pointyness, alength, blength):
+    blength += thickness
+
+    midpoint_a = _midpoint(Point(space, 0), Point(space, thickness))
+    arraw_a = _move(midpoint_a, 0, pointyness)
+
+    midpoint_b = _midpoint(Point(alength, blength), Point(alength - thickness, blength))
+    arraw_b = _move(midpoint_b, 90, pointyness)
+
+    points = [
+        Point(space, 0),
+        Point(alength, 0),
+        Point(alength, blength),
+        arraw_b,
+        Point(alength - thickness, blength),
+        Point(alength - thickness, thickness),
+        Point(space, thickness),
+        arraw_a
+    ]
+
+    return _create_polygon(points)
 
 
 def create(thickness=17, offset=30, space=10, pointedness=7):
@@ -87,16 +114,41 @@ def create(thickness=17, offset=30, space=10, pointedness=7):
     result += '<g transform="translate(100,100) rotate(180) translate(0, {})">'.format((100 - logo_height) / 2)
 
     # Create one side
-    result += _create_polygon(thickness, offset, space, pointedness)
+    result += _create_triangle_side(thickness, offset, space, pointedness)
 
     # Create rotated second side
     result += '<g transform="translate(100,0) rotate(120) ">'
-    result += _create_polygon(thickness, offset, space, pointedness)
+    result += _create_triangle_side(thickness, offset, space, pointedness)
     result += '</g>'
 
     # Create the final piece
     result += '<g transform="translate(50,{}) rotate(240) ">'.format(logo_height)
-    result += _create_polygon(thickness, offset, space, pointedness)
+    result += _create_triangle_side(thickness, offset, space, pointedness)
+    result += '</g>'
+
+    result += "</g></svg>"
+    return result
+
+
+def create_rectangular(thickness=17, width=50, offset=17, space=5, pointedness=7):
+    result = '<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">'
+    result += '<g transform="translate({}, 0)">'.format((100 - width) / 2)
+
+    result += _create_square_side(thickness, offset, space, pointedness, width, 0)
+
+    # Create rotated second side
+    result += '<g transform="translate({},{}) rotate(90) ">'.format(width, thickness)
+    result += _create_square_side(thickness, offset, space, pointedness, 100 - thickness, 0)
+    result += '</g>'
+
+    # Create rotated second side
+    result += '<g transform="translate({},{}) rotate(180) ">'.format(2 * thickness, 100)
+    result += _create_square_side(thickness, offset, space, pointedness, width, 0)
+    result += '</g>'
+
+    # Create rotated second side
+    result += '<g transform="translate({},{}) rotate(270) ">'.format(-thickness, 100-thickness)
+    result += _create_square_side(thickness, offset, space, pointedness, 100 - thickness, 0)
     result += '</g>'
 
     result += "</g></svg>"
