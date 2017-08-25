5258e49ebd3659c70df2a3e306442ee2934d3fc2
Martijn Braam <martijn@brixit.nl>
2017-08-25 12:31:27 +0200

    Added phone frame

--- logo/__init__.py
+++ logo/__init__.py
@@ -97,7 +97,64 @@ def _create_square_side(thickness, offset, space, pointyness, alength, blength):
     return _create_polygon(points)
 
 
-def create(thickness=17, offset=30, space=10, pointedness=7):
+def _create_path(commands, color):
+    d = ""
+    for command in commands:
+        d += str(command) + " "
+    return '<path d="{}" fill="{}" />'.format(d.strip(), color)
+
+
+def _create_phone(width, height, radius, bezel):
+    result = ""
+    commands = []
+    # starting point
+    commands.append('M 0 {}'.format(radius))
+    # top left corner
+    commands.append('A {radius} {radius} 0 0 1 {x} {y}'.format(radius=radius, x=radius, y=0))
+    # top line
+    commands.append('h {}'.format(width - radius - radius))
+    # top right corner
+    commands.append('a {radius} {radius} 0 0 1 {x} {y}'.format(radius=radius, x=radius, y=radius))
+    # right line
+    commands.append('v {}'.format(height - radius - radius))
+    # bottom right corner
+    commands.append('a {radius} {radius} 0 0 1 {x} {y}'.format(radius=radius, x=-radius, y=radius))
+    # bottom line
+    commands.append('h {}'.format(-(width - radius - radius)))
+    # bottom left corner
+    commands.append('a {radius} {radius} 0 0 1 {x} {y}'.format(radius=radius, x=-radius, y=-radius))
+    # close shape
+    commands.append('Z')
+
+    result += _create_path(commands, '#000')
+
+    # Draw the screen
+    result += '<rect x="{}" y="{}" width="{}" height="{}" fill="{}"/>'.format(bezel, radius, width - bezel - bezel,
+                                                                              height - radius - radius, '#fff')
+
+    hole_radius = radius / 2
+    hole_width = width / 3
+
+    commands = []
+    commands.append('M {} {}'.format(hole_width + hole_radius, hole_radius * 1.5))
+    commands.append('a {radius} {radius} 0 1 1 {x} {y}'.format(radius=hole_radius / 2, x=0, y=-hole_radius))
+    commands.append('h {}'.format(hole_width - hole_radius - hole_radius))
+    commands.append('a {radius} {radius} 0 1 1 {x} {y}'.format(radius=hole_radius / 2, x=0, y=hole_radius))
+    commands.append('Z')
+    result += _create_path(commands, '#fff')
+
+    commands = []
+    commands.append('M {} {}'.format(hole_width + hole_radius, height - (hole_radius * 0.5)))
+    commands.append('a {radius} {radius} 0 1 1 {x} {y}'.format(radius=hole_radius / 2, x=0, y=-hole_radius))
+    commands.append('h {}'.format(hole_width - hole_radius - hole_radius))
+    commands.append('a {radius} {radius} 0 1 1 {x} {y}'.format(radius=hole_radius / 2, x=0, y=hole_radius))
+    commands.append('Z')
+    result += _create_path(commands, '#fff')
+
+    return result
+
+
+def create(thickness=17, offset=30, space=10, pointedness=7, phone=True):
     """ Create logo svg image
 
     :param thickness: Thickness of the line
@@ -110,8 +167,16 @@ def create(thickness=17, offset=30, space=10, pointedness=7):
 
     result = '<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">'
 
+    logo_translate = ""
+    if phone:
+        result += '<g transform="translate(20,0)">'
+        result += _create_phone(60, 100, 10, 5)
+        result += '</g>'
+        logo_translate = "scale(0.5) translate(50,50)"
+
     # Shift whole logo downward since a equilateral triangle doesn't fit in a square box
-    result += '<g transform="translate(100,100) rotate(180) translate(0, {})">'.format((100 - logo_height) / 2)
+    result += '<g transform="translate(100,100) rotate(180) translate(0, {}){}">'.format((100 - logo_height) / 2,
+                                                                                         logo_translate)
 
     # Create one side
     result += _create_triangle_side(thickness, offset, space, pointedness)
@@ -147,7 +212,7 @@ def create_rectangular(thickness=17, width=50, offset=17, space=5, pointedness=7
     result += '</g>'
 
     # Create rotated second side
-    result += '<g transform="translate({},{}) rotate(270) ">'.format(-thickness, 100-thickness)
+    result += '<g transform="translate({},{}) rotate(270) ">'.format(-thickness, 100 - thickness)
     result += _create_square_side(thickness, offset, space, pointedness, 100 - thickness, 0)
     result += '</g>'
 
