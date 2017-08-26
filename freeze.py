f51b6f742a115ae2914629d02cda217b9863da3a
Oliver Smith <ollieparanoid@bitmessage.ch>
2017-08-26 17:28:10 +0200

    Update redirects

--- freeze.py
+++ freeze.py
@@ -2,9 +2,11 @@ from flask_frozen import Freezer
 from app import app, BLOG_CONTENT_DIR
 from os import listdir
 
+
 freezer = Freezer(app)
 app.config['FREEZER_DESTINATION'] = 'docs'
 app.config['FREEZER_BASE_URL'] = 'https://postmarketos.org/'
+
 
 @freezer.register_generator
 def blog_post():
@@ -12,6 +14,13 @@ def blog_post():
         y, m, d, *title = f[:-3].split('-')
         slug = '-'.join(title)
         yield { 'y': y, 'm': m, 'd': d, 'slug': slug }
+
+
+@freezer.register_generator
+def wiki_redirects():
+    for slug, redirect in WIKI_REDIRECTS.items():
+        yield {'slug': slug }
+
 
 if __name__ == '__main__':
     freezer.freeze()
